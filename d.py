8db3e044f09b6e00a4eeb115a8c22eb58f875aa9
Erjan18 <era.kar02@gmail.com>
2025-02-05 13:03:55 +0600

    uni

--- d.py
+++ d.py
@@ -1,2 +1,15 @@
-def v():
-    pass
+def name():
+    var = 14
+    val = 224
+    return var + val
+print(name())
+
+def name1(var,val):
+    return var + val
+print(name1(12,12))
+def name1(var,val):
+    return var + val
+print(name1(12,12))
+def name1(var,val):
+    return var + val
+print(name1(12,12))
--- def.py
+++ def.py
@@ -1,20 +1,24 @@
-# def kalkulyator():
-#     a=int(input("give me first number"))
-#     c = input("give me znak:")
-#     print("вы можете ввести знаки :+,-,/,*")
-#     b=int(input("give me second number"))
-#     if c== "+":
-#         return a+b
-#     elif c== "-":
-#         return a-b
-#     elif c =="/":
-#         return a/b
-#     elif c == "*":
-#         return a*b
-#     else:
-#          print("error from mirror")
-# print(kalkulyator())
-#
-#
+def kalkulyator():
+    a=int(input("give me first number"))
+    c = input("give me znak:")
+    print("вы можете ввести знаки :+,-,/,*")
+    b=int(input("give me second number"))
+    if c== "+":
+        return a+b
+    elif c== "-":
+        return a-b
+    elif c =="/":
+        return a/b
+    elif c == "*":
+        return a*b
+    else:
+         print("error from mirror")
+print(kalkulyator())
+
+
+
+
+
+
 
 
